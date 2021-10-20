74168d71a33084db953f53d704c3a9d03cf1f7f0
ShairbekovBakyt <cyberstalker0726@gmail.com>
2021-10-20 01:39:02 -0400

    features/ticket4

--- seo/admin.py
+++ seo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
+admin.site.register(Blog)
+admin.site.register(Category)
 # Register your models here.
--- seo/models.py
+++ seo/models.py
@@ -1,10 +1,48 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User
 
 class Blog(models.Model):
-    title = models.CharField(max_length=160)
-    text = models.TextField()
-    image = models.ImageField()
-    publish_date = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    is_published = models.BooleanField(default=False)
+    title = models.CharField(
+        max_length=160,
+        verbose_name='Заголовок',
+        db_index=True)
+    text = models.TextField(
+        blank=True,
+        verbose_name='Контент')
+    image = models.ImageField(
+        upload_to='image/%Y/%m/%d',
+        verbose_name='Изображение')
+    publish_date = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата Публикации')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.DO_NOTHING,
+        verbose_name='Автор')
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Состаяние Публикации')
+    category = models.ForeignKey(
+        "Category",
+        on_delete=models.DO_NOTHING,
+        verbose_name='Категория')
+    class Meta:
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блоги'
+        ordering = ['-publish_date']
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    title = models.CharField(
+        max_length=220,
+        verbose_name='Категории')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['-title']
+
+    def __str__(self):
+        return self.title

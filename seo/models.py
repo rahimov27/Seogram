964e5e5e7cee6925d6414148c83b034c60f9977c
ShairbekovBakyt <cyberstalker0726@gmail.com>
2021-10-20 00:43:18 -0400

    merged/dev

--- seo/models.py
+++ seo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=160)
+    text = models.TextField()
+    image = models.ImageField()
+    publish_date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    is_published = models.BooleanField(default=False)

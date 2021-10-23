4d02e44ed076c40fd401416affac26d94dcc3179
ktemirbekovna <tktemirbekovna@gmail.com>
2021-10-23 14:08:09 +0600

    bugfix/add_categories_and_templates

--- seo/urls.py
+++ seo/urls.py
@@ -6,10 +6,10 @@ from Seogram import settings
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('post-detail/<str:slug>', PostDetail.as_view(), name='post-detail'),
-    path('about/',aboutus,name='about'),
-    path('blog_list/',blog_list,name='blog-list'),
+    path('post-detail/<str:slug>', Blog_Detail.as_view(), name='post-detail'),
+    path('about/',Aboutus.as_view(),name='about'),
+    path('blog_list/',Blog_list.as_view(),name='blog-list'),
     path('contact/',contact,name='contact'),
-    path('service/',service,name='service'),
+    path('service/',Service.as_view(),name='service'),
     path('category/<str:slug>', get_category, name='category'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- seo/views.py
+++ seo/views.py
@@ -1,48 +1,59 @@
 from django.shortcuts import render, redirect
 from .forms import *
 from .models import Blog
-from django.views.generic import ListView, DetailView
-
+from django.views.generic import ListView,DetailView
 
 class Home(ListView):
     model = Blog
     template_name = 'seo/index.html'
 
-class BlogList(ListView):
+class Blog_list(ListView):
     model = Blog
     template_name = 'seo/blog.html'
     context_object_name = 'posts'
     paginate_by = 9
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = super().get_context_data()
         context['title'] = 'Blog'
         return context
 
 
-class PostDetail(DetailView):
+class Blog_Detail(DetailView):
     model = Blog
-    template_name = 'seo/blog-detail.html'
+    template_name = 'seo/blog-details.html'
     context_object_name = 'post'
-
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Blog'
+        context = super().get_context_data()
+        context['title'] = 'post'
         return context
 
-def get_category(request, slug):
-    return render(request, 'blog/category.html')
+class Service(ListView):
+    model = Blog
+    template_name = 'seo/service.html'
 
-def blog_list(request):
-    post = Blog.objects.all()
-    context = {
-        'posts' : post
-    }
-    return render(request, template_name='seo/blog.html', context=context)
+class Aboutus(ListView):
+    model = Blog
+    template_name = 'seo/about.html'
 
-def aboutus(request):
-    return render(request, template_name='seo/about.html')
+def get_category(request,slug):
+    return render(request,'seo/category.html')
 
+# class Contact(request,ListView):
+#     model = ContactForm
+#     template_name = 'seo/contact.html'
+#     context_object_name = 'form'
+#     if request.method == 'POST':
+#         form = ContactForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('/')
+#     else:
+#         form = ContactForm()
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data()
+#         context['first_name'] = 'Bakyt'
+#         return context
 
 def contact(request):
     if request.method == 'POST':
@@ -56,6 +67,3 @@ def contact(request):
             'form': form
         }
     return render(request, template_name='seo/contact.html', context=context)
-
-def service(request):
-    return render(request, template_name='seo/service.html')
